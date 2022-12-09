460ea96f4e32028a3047d9774a2057975012c00f
shir <shirfintsy@gmail.com>
2022-12-09 11:30:41 +0200

    eyeball part almost finished

--- src/game/eyeBalls.js
+++ src/game/eyeBalls.js
@@ -1,25 +1,36 @@
 
 import './eyeBalls.css'
+import {createRef, useEffect, useState} from "react";
+import Button from "react-bootstrap/Button";
 
 function EyeBalls() {
+    const [eyeMovement, changeMove] = useState(false);
 
+    const eye = createRef();
+    const clicker = () => {
+        const look = eye.current;
+        look.classList.toggle('after-look');
+    }
 
     // useEffect(() => {
+    //     const look = eye.current;
+    //     // if (eyeMovement) {
+    //     //     look.classList.toggle('after-look')
+    //     // }
+    //     look.classList.toggle('after-look');
     //     setTimeout(() => {
-    //         setSide(!side);
-    //         // if (side) {
-    //         //
-    //         // }
-    //     }, 2500);
-    // }, [side]);
+    //         changeMove(!eyeMovement);
+    //     }, 2000);
+    // }, [eyeMovement]);
 
     return (
         <div className={"all-robot"} style={{ backgroundImage: "url(/cute_robot2.jpg)", backgroundRepeat  : 'no-repeat',
        backgroundPosition: 'center', backgroundSize: 'contain', width: 700, height: 700, display: 'block'}}>
         <div className="container">
-            <div className="eyes"></div>
+            <div ref={eye} className="eyes"></div>
             <div className="eyes"></div>
         </div>
+            <Button onClick={()=> clicker()}> </Button>
         </div>
     )
 } export default EyeBalls;
