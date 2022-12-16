c82e8eeead6b7f7e8c4409ebe2859f14aedf6080
shir <shirfintsy@gmail.com>
2022-12-16 12:15:13 +0200

    finshed design the help request page

--- src/game/mainHelpRequestPage.js
+++ src/game/mainHelpRequestPage.js
@@ -6,13 +6,15 @@ import {ImageList, ImageListItem, ImageListItemBar} from "@mui/material";
 
 function MainHelpRequestPage({profilePicture, username, onClickYes, onClickNo}) {
     const [popButtons, setPop] = useState(false);
-    let srcImage = "https://t3.ftcdn.nbyet/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg";
+    let srcImage = null;
     if (profilePicture === "default") { //todo: change with real images
         srcImage = "https://t3.ftcdn.nbyet/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg";
     } else if (profilePicture === "woman1") {
-        srcImage = "https://img.freepik.com/premium-vector/woman-profile-cartoon_18591-58480.jpg?w=2000";
+        //srcImage = "https://img.freepik.com/premium-vector/woman-profile-cartoon_18591-58480.jpg?w=2000";
+        srcImage = "profile_pictures/woman1.webp";
     } else {
-        srcImage = "https://thumbs.dreamstime.com/b/man-profile-cartoon-smiling-round-icon-vector-illustration-graphic-design-135443422.jpg";
+        //srcImage = "https://thumbs.dreamstime.com/b/man-profile-cartoon-smiling-round-icon-vector-illustration-graphic-design-135443422.jpg";
+        srcImage = "profile_pictures/man1.jpeg"
     }
 
     const itemData = [
@@ -34,8 +36,8 @@ function MainHelpRequestPage({profilePicture, username, onClickYes, onClickNo}) 
     }, []);
     return (
         // <div style={{ backgroundImage: "url(/man_and_robot.png)" }}>
-        <div>
-            <div id={"robot-eyes"}>
+        <div >
+            <div id={"robot-eyes"} style={{"border": 'double', "margin": '20px'}}>
                 <EyeBalls/>
                 {/*<img alt={"eye-robot-image"} src={"radio-bot-animated.gif"}/>*/}
             </div>
